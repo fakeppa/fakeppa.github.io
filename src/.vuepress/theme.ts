c9e3736fab105c72379cf526936c139542099f17
fakeppa <l17091799155@qq.com>
2024-12-11 21:41:05 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -13,7 +13,7 @@ export default hopeTheme({
 
   iconAssets: "fontawesome-with-brands",
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
+  logo: "https://cdn.jsdelivr.net/gh/fakeppa/blog-img/K13134131.svg",
 
   repo: "fakeppa/fakeppa.github.io",
 
