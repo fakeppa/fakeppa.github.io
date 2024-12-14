b16c63930a8d91975f6e7a60da71502896cf0beb
fakeppa <l17091799155@qq.com>
2024-12-14 14:52:59 +0800

    Update navbar.ts

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,59 +3,59 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/",
   "/intro",
-  {
-    text: "才疏学浅",
-    icon: "pen-to-square",
-    prefix: "/article/",
-    children: [
-      {
-        text: "项目",
-        icon: "pen-to-square",
-        prefix: "01_项目",
-        children: [
-          {
-            text: "项目简介",
-            icon: "pen-to-square",
-            link: "项目--简介",
-          },
-        ],
-      },
-      {
-        text: "领域",
-        icon: "pen-to-square",
-        prefix: "02_领域",
-        children: [
-          {
-            text: "领域简介",
-            icon: "pen-to-square",
-            link: "领域--简介",
-          },
-        ],
-      },
-      {
-        text: "资源",
-        icon: "pen-to-square",
-        prefix: "03_资源",
-        children: [
-          {
-            text: "资源简介",
-            icon: "pen-to-square",
-            link: "资源--简介",
-          },
-        ],
-      },
-      {
-        text: "归档",
-        icon: "pen-to-square",
-        prefix: "04_归档",
-        children: [
-          {
-            text: "归档简介",
-            icon: "pen-to-square",
-            link: "归档--简介",
-          },
-        ],
-      },
-    ],
-  },
+  //{
+   // text: "才疏学浅",
+    //icon: "pen-to-square",
+    //prefix: "/article/",
+    //children: [
+      //{
+        //text: "项目",
+        //icon: "pen-to-square",
+        //prefix: "01_项目",
+        //children: [
+          //{
+            //text: "项目简介",
+            //icon: "pen-to-square",
+            //link: "项目--简介",
+          //},
+        //],
+      //},
+      //{
+        //text: "领域",
+        //icon: "pen-to-square",
+        //prefix: "02_领域",
+        //children: [
+          //{
+            //text: "领域简介",
+            //icon: "pen-to-square",
+            //link: "领域--简介",
+          //},
+        //],
+      //},
+      //{
+        //text: "资源",
+        //icon: "pen-to-square",
+        //prefix: "03_资源",
+       // children: [
+          //{
+            //text: "资源简介",
+            //icon: "pen-to-square",
+            //link: "资源--简介",
+          //},
+        //],
+      //,
+      //{
+        //text: "归档",
+        //icon: "pen-to-square",
+        //prefix: "04_归档",
+        //children: [
+          //{
+            //text: "归档简介",
+            //icon: "pen-to-square",
+            //link: "归档--简介",
+          //},
+        //],
+      //},
+    //],
+  //},
 ]);
